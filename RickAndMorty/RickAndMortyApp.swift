f45016a6d1d9531cea3b13a3b7c063726b7769cb
dsm5e <justry24@gmail.com>
2023-08-16 21:33:41 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by dsm 5e on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
